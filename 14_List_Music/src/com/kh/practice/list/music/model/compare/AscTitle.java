a2291bda50ee2209395ea01fa49d8f472d6fc22d
LIMJIYEON59 <chayi2110@gmail.com>
2023-06-07 17:55:56 +0900

    Music 실습

---
+++ 14_List_Music/src/com/kh/practice/list/music/model/compare/AscTitle.java
@@ -0,0 +1,12 @@
+package com.kh.practice.list.music.model.compare;
+
+public class AscTitle {
+	public int compare(Object o,  Object o2) { //o,o2 숫자로 시작 못함
+		int result = 0;
+		//TODO
+		return result; //int형이라 return 꼭 해야함
+		
+		
+	}
+
+}
